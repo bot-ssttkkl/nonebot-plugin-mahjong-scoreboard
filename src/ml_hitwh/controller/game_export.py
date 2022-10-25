7021a6eadc5e1d83a8da71459681363a38b14a09
ssttkkl <huang.wen.long@hotmail.com>
2022-10-25 18:42:22 +0800

    添加导出所有对局命令

--- src/ml_hitwh/controller/game_export.py
+++ src/ml_hitwh/controller/game_export.py
@@ -3,7 +3,6 @@ from io import StringIO
 
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent
-# ========== 导出赛季对局 ==========
 from nonebot.internal.matcher import Matcher
 
 from ml_hitwh.controller.file_center import send_group_file, send_private_file
@@ -14,27 +13,28 @@ from ml_hitwh.controller.workflow_general import require_group_binding_qq
 from ml_hitwh.errors import BadRequestError
 from ml_hitwh.model.enums import SeasonState
 from ml_hitwh.service import season_service
-from ml_hitwh.service.game_service import get_season_games
+from ml_hitwh.service.game_service import get_season_games, get_group_games
 from ml_hitwh.service.group_service import get_group_by_binding_qq
 from ml_hitwh.utils import encode_date
 
-export_season_game_matcher = on_command("导出赛季对局", aliases={"导出对局"}, priority=5)
+# ========== 导出赛季对局 ==========
+export_season_games_matcher = on_command("导出赛季对局", aliases={"导出对局"}, priority=5)
 
 
-@export_season_game_matcher.handle()
-@workflow_interceptor(export_season_game_matcher)
-async def export_season_game_begin(event: MessageEvent, matcher: Matcher):
+@export_season_games_matcher.handle()
+@workflow_interceptor(export_season_games_matcher)
+async def export_season_games_begin(event: MessageEvent, matcher: Matcher):
     args = split_message(event.message)
     if len(args) > 1 and args[1].type == 'text':
         matcher.state["season_code"] = args[1].data["text"]
 
 
-require_group_binding_qq(export_season_game_matcher)
+require_group_binding_qq(export_season_games_matcher)
 
 
-@export_season_game_matcher.handle()
-@general_interceptor(export_season_game_matcher)
-async def export_season_game(bot: Bot, event: MessageEvent, matcher: Matcher):
+@export_season_games_matcher.handle()
+@general_interceptor(export_season_games_matcher)
+async def export_season_games(bot: Bot, event: MessageEvent, matcher: Matcher):
     group = await get_group_by_binding_qq(matcher.state["binding_qq"])
 
     season_code = matcher.state.get("season_code", None)
@@ -57,6 +57,40 @@ async def export_season_game(bot: Bot, event: MessageEvent, matcher: Matcher):
         now = datetime.now()
         filename += f"（截至{encode_date(now)}）"
     filename += ".csv"
+
+    with StringIO() as sio:
+        await map_games_as_csv(sio, games)
+
+        data = sio.getvalue().encode("utf_8_sig")
+        if isinstance(event, GroupMessageEvent):
+            await send_group_file(bot, event.group_id, filename, data)
+        else:
+            await send_private_file(bot, event.user_id, filename, data)
+
+
+# ========== 导出所有对局 ==========
+export_group_games_matcher = on_command("导出所有对局", aliases={"导出群对局"}, priority=5)
+
+
+@export_group_games_matcher.handle()
+@workflow_interceptor(export_group_games_matcher)
+async def export_group_games_begin(event: MessageEvent, matcher: Matcher):
+    args = split_message(event.message)
+    if len(args) > 1 and args[1].type == 'text':
+        matcher.state["season_code"] = args[1].data["text"]
+
+
+require_group_binding_qq(export_group_games_matcher)
+
+
+@export_group_games_matcher.handle()
+@general_interceptor(export_group_games_matcher)
+async def export_group_games(bot: Bot, event: MessageEvent, matcher: Matcher):
+    group = await get_group_by_binding_qq(matcher.state["binding_qq"])
+    games = await get_group_games(group)
+
+    now = datetime.now()
+    filename = f"所有对局（截至{encode_date(now)}）.csv"
 
     with StringIO() as sio:
         await map_games_as_csv(sio, games)
