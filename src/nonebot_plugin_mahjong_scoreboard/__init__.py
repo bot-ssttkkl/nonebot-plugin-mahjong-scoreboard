411768f75e1b58890d9cdf507950051e7f4b2cb7
ssttkkl <huang.wen.long@hotmail.com>
2022-10-29 08:38:42 +0800

    添加帮助文档

--- src/nonebot_plugin_mahjong_scoreboard/__init__.py
+++ src/nonebot_plugin_mahjong_scoreboard/__init__.py
@@ -5,38 +5,39 @@ nonebot-plugin-mahjong-scoreboard
 @License        : MIT
 @GitHub         : https://github.com/ssttkkl/nonebot-plugin-mahjong-scoreboard
 """
+from .utils.onebot import default_cmd_start
 
-help_text = """
+help_text = f"""
 对局：
-- 新建对局 [四人南|四人东]
-- 结算对局 <成绩> [对局<编号>] [@<用户>] [<自风>]
-- 撤销结算对局 [对局<编号>] [@<用户>]
-- 设置对局PT <PT> [对局<编号>] [@<用户>]
-- 删除对局 [对局<编号>]
-- 设置对局进度 <进度> [对局<编号>]
+- {default_cmd_start}新建对局 [四人南|四人东]
+- {default_cmd_start}结算对局 <成绩> [对局<编号>] [@<用户>] [<自风>]
+- {default_cmd_start}撤销结算对局 [对局<编号>] [@<用户>]
+- {default_cmd_start}设置对局PT <PT> [对局<编号>] [@<用户>]
+- {default_cmd_start}删除对局 [对局<编号>]
+- {default_cmd_start}设置对局进度 <进度> [对局<编号>]
 
 对局查询：
-- 查询对局 [<编号>]
-- 个人最近对局 [@<用户>]
-- 群最近对局
-- 个人未完成对局 [@<用户>]
-- 群未完成对局
-- 导出赛季对局 [<代号>]
-- 导出所有对局
+- {default_cmd_start}查询对局 [<编号>]
+- {default_cmd_start}个人最近对局 [@<用户>]
+- {default_cmd_start}群最近对局
+- {default_cmd_start}个人未完成对局 [@<用户>]
+- {default_cmd_start}群未完成对局
+- {default_cmd_start}导出赛季对局 [<代号>]
+- {default_cmd_start}导出所有对局
 
 赛季：
-- 查询赛季 [<代号>]
-- 查询所有赛季
-- 新建赛季
-- 开启赛季 [<代号>]
-- 结束赛季
-- 删除赛季 [<代号>]
+- {default_cmd_start}查询赛季 [<代号>]
+- {default_cmd_start}查询所有赛季
+- {default_cmd_start}新建赛季
+- {default_cmd_start}开启赛季 [<代号>]
+- {default_cmd_start}结束赛季
+- {default_cmd_start}删除赛季 [<代号>]
 
 赛季PT：
-- 设置赛季PT <PT> [@<用户>]
-- 查询PT [@<用户>]
-- 查询榜单
-- 导出榜单
+- {default_cmd_start}设置赛季PT <PT> [@<用户>]
+- {default_cmd_start}查询PT [@<用户>]
+- {default_cmd_start}查询榜单
+- {default_cmd_start}导出榜单
 
 以上命令格式中，以<>包裹的表示一个参数，以[]包裹的表示一个可选项。
 
