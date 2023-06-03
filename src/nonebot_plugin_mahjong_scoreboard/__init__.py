1680fdbe7e8f652a8eaf618fd6d7f288ad2d51d3
ssttkkl <huang.wen.long@hotmail.com>
2023-06-03 09:17:08 +0800

    更新插件元信息

--- src/nonebot_plugin_mahjong_scoreboard/__init__.py
+++ src/nonebot_plugin_mahjong_scoreboard/__init__.py
@@ -13,12 +13,12 @@ require("nonebot_plugin_sqlalchemy")
 require("nonebot_plugin_apscheduler")
 require("nonebot_plugin_gocqhttp_cross_machine_upload_file")
 
+from .config import Config
 from .controller import game_record, game_query, game_export, season_query, season_manage, season_user_point_query, \
     season_user_point_export, season_user_point_manage, game_statistics
-
 from .utils.nonebot import default_cmd_start
 
-help_text = f"""
+__usage__ = f"""
 对局：
 - {game_record.new_game_matcher.__help_info__}
 - {game_record.record_matcher.__help_info__}
@@ -38,19 +38,19 @@ help_text = f"""
 - {game_export.export_group_games_matcher.__help_info__}
 
 赛季：
-- {season_query.query_season_matcher}
-- {season_query.query_all_seasons_matcher}
-- {season_manage.new_season_matcher}
-- {season_manage.start_season_matcher}
-- {season_manage.finish_season_matcher}
-- {season_manage.remove_season_matcher}
+- {season_query.query_season_matcher.__help_info__}
+- {season_query.query_all_seasons_matcher.__help_info__}
+- {season_manage.new_season_matcher.__help_info__}
+- {season_manage.start_season_matcher.__help_info__}
+- {season_manage.finish_season_matcher.__help_info__}
+- {season_manage.remove_season_matcher.__help_info__}
 
 赛季查询：
-- {season_user_point_query.query_season_ranking_matcher}
-- {season_user_point_export.export_season_ranking_matcher}
-- {season_user_point_query.query_season_point_matcher}
-- {season_user_point_manage.set_season_user_point_matcher}
-- {season_user_point_manage.reset_season_user_point_matcher}
+- {season_user_point_query.query_season_ranking_matcher.__help_info__}
+- {season_user_point_export.export_season_ranking_matcher.__help_info__}
+- {season_user_point_query.query_season_point_matcher.__help_info__}
+- {season_user_point_manage.set_season_user_point_matcher.__help_info__}
+- {season_user_point_manage.reset_season_user_point_matcher.__help_info__}
 
 数据统计：
 - {game_statistics.query_user_statistics_matcher}
@@ -67,7 +67,11 @@ from nonebot.plugin import PluginMetadata
 __plugin_meta__ = PluginMetadata(
     name='日麻寄分器',
     description='为群友提供日麻计分及榜单统计功能',
-    usage=help_text
+    usage=__usage__,
+    type="application",
+    homepage="https://github.com/ssttkkl/nonebot-plugin-mahjong-scoreboard",
+    config=Config,
+    supported_adapters={"~onebot.v11", "~qqguild"}
 )
 
-__all__ = ("__plugin_meta__",)
+__all__ = ("__plugin_meta__", "__usage__")
