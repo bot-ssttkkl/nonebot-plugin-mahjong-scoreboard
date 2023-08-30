2135a3dc24787188fb7f487c45ab2b24c5b537ac
ssttkkl <huang.wen.long@hotmail.com>
2023-08-30 21:04:28 +0800

    修复迁移错误

--- src/nonebot_plugin_mahjong_scoreboard/repository/_data_source/migrations/v3_to_v4.py
+++ src/nonebot_plugin_mahjong_scoreboard/repository/_data_source/migrations/v3_to_v4.py
@@ -11,11 +11,14 @@ async def migrate_v3_to_v4():
     async with AsyncSession(data_source.engine) as sess:
         result = await sess.execute(select(SeasonOrm.id, SeasonOrm.config))
         for (id_, config) in result.all():
+            scale = 10 ** -config.point_precision
             config.rank_point_policy = RankPointPolicy.horse_point
             if config.south_game_enabled:
                 config.south_game_initial_point = 25000
+                config.south_game_horse_point = [x * scale for x in config.south_game_horse_point]
             if config.east_game_enabled:
                 config.east_game_initial_point = 25000
+                config.east_game_horse_point = [x * scale for x in config.east_game_horse_point]
 
             stmt = update(SeasonOrm).where(SeasonOrm.id == id_).values(config=config)
             await sess.execute(stmt)
