9d98c18ccf15c825df96775a885adea612cd3985
ssttkkl <huang.wen.long@hotmail.com>
2024-10-20 00:06:31 +0800

    兼容pydantic-v2

--- src/nonebot_plugin_mahjong_scoreboard/config.py
+++ src/nonebot_plugin_mahjong_scoreboard/config.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from typing import Any
 
 import nonebot_plugin_localstore as store
-from nonebot import get_driver
-from pydantic import BaseSettings, root_validator
+from nonebot import get_plugin_config
+from nonebot.compat import PYDANTIC_V2
+from pydantic import BaseModel
 
 
 def _get_default_sql_conn_url():
@@ -14,14 +16,29 @@ def _get_default_sql_conn_url():
     return "sqlite+aiosqlite:///" + str(data_file)
 
 
-class Config(BaseSettings):
+def compatible_model_pre_validator(func):
+    if PYDANTIC_V2:
+        from pydantic import model_validator
+
+        @model_validator(mode='before')
+        @classmethod
+        def checker(cls, values: dict[str]):
+            return func(cls, values)
+
+        return checker
+    else:
+        from pydantic import root_validator
+        return root_validator(pre=True, allow_reuse=True)(func)
+
+
+class Config(BaseModel):
     mahjong_scoreboard_database_conn_url: str
     mahjong_scoreboard_send_forward_message: bool = True
     mahjong_scoreboard_send_image: bool = False
     mahjong_scoreboard_enable_permission_check: bool = True
 
-    @root_validator(pre=True, allow_reuse=True)
-    def default_sql_conn_url(cls, values):
+    @compatible_model_pre_validator
+    def default_sql_conn_url(cls, values: dict[str, Any]):
         if "mahjong_scoreboard_database_conn_url" not in values:
             values["mahjong_scoreboard_database_conn_url"] = _get_default_sql_conn_url()
         return values
@@ -30,4 +47,4 @@ class Config(BaseSettings):
         extra = "ignore"
 
 
-conf = Config(**get_driver().config.dict())
+conf = get_plugin_config(Config)
